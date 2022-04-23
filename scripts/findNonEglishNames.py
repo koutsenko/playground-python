cab7509a94ea38de8d5eb8c68436d1b38f9dd198
Koutsenko Dmitry <koutsenko@gmail.com>
2022-04-23 17:05:35 +0300

    Script that search for folders and files with non-english names

---
+++ scripts/findNonEglishNames.py
@@ -0,0 +1,26 @@
+﻿import os
+
+
+def find_wrong(path):
+    wrong = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if not file.isascii():
+                wrong.append(os.path.join(root, file))
+        for directory in dirs:
+            if not directory.isascii():
+                wrong.append(os.path.join(root, directory))
+            wrong.extend(find_wrong(directory))
+    return wrong
+
+
+def main():
+    wrong = find_wrong("D:\\notes\\")
+    print(f"Found {len(wrong)} names:")
+    for w in wrong:
+        print(w)
+    print("Done!")
+
+
+if __name__ == "__main__":
+    main()
